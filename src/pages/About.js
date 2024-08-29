85cac4ed824e4007e78db3e1b96102e06b598153
Ammar <ammarhussain0315@gmail.com>
2024-08-29 18:16:55 +0500

    updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,20 +14,21 @@ function About() {
         <div className='w-[90%] flex flex-col md:flex-row gap-5 md:gap-0 '>
           <div className='w-[100%] md:w-[50%] flex flex-col gap-10'>
             <p className='font-poppins text-[16px] md:text-[18px]'>
-              Aptech Pakistan is a premier institute for technical education,
-              dedicated to providing students with the knowledge and skills they
-              need to succeed in the ever-evolving world of technology. With
-              over 30 years of experience in designing courses and providing
-              topnotch education to students, Aptech Pakistan is the go-to
-              choice for students who want to excel in their careers.
+              Nixortech Solutions is a dynamic and forward-thinking institution
+              dedicated to empowering individuals through cutting-edge technical
+              education. Our focus is on providing students with the practical
+              skills and knowledge they need to excel in the fast-paced world of
+              technology. With a commitment to innovation and quality education,
+              Nixortech Solutions is paving the way for future tech leaders.
             </p>
             <p className='font-poppins text-[16px] md:text-[18px]'>
-              At Aptech Pakistan, we offer a wide range of technical education
-              courses, including Information Technology, Multimedia & Animation,
-              Hardware & Networking, and Aviation & Hospitality. Our courses are
-              designed to provide students with a rigorous and challenging
-              learning experience that prepares them for the real-world
-              challenges they will face in their careers.
+              At Nixortech Solutions, we offer a wide array of courses including
+              Artificial Intelligence, Mobile Application Development, Web
+              Development, Graphic Designing, Cyber Security, and more. Our
+              programs are carefully designed to meet the demands of the
+              ever-evolving tech industry, ensuring our students are equipped
+              with the latest skills and industry insights to succeed in their
+              careers.
             </p>
           </div>
           <div className='w-[100%] md:w-[50%] flex justify-center items-center'>
@@ -44,24 +45,22 @@ function About() {
           </div>
           <div className='w-[100%] md:w-[50%] flex flex-col gap-10'>
             <h1 className='text-[34px] md:text-[44px] font-bold font-poppins'>
-              Aptech Pakistan
+              Nixortech
             </h1>
 
             <p className='font-poppins text-[16px] md:text-[18px]'>
-              Aptech Pakistan is a premier institute for technical education,
-              dedicated to providing students with the knowledge and skills they
-              need to succeed in the ever-evolving world of technology. With
-              over 30 years of experience in designing courses and providing
-              topnotch education to students, Aptech Pakistan is the go-to
-              choice for students who want to excel in their careers.
+              Nixortech Solutions is dedicated to excellence in education,
+              striving to deliver top-quality training that is customer-centric
+              and industry-relevant. Our commitment to excellence ensures that
+              our students are not just skilled professionals but also future
+              leaders in the global technology space.
             </p>
             <p className='font-poppins text-[16px] md:text-[18px]'>
-              At Aptech Pakistan, we offer a wide range of technical education
-              courses, including Information Technology, Multimedia & Animation,
-              Hardware & Networking, and Aviation & Hospitality. Our courses are
-              designed to provide students with a rigorous and challenging
-              learning experience that prepares them for the real-world
-              challenges they will face in their careers.
+              Our mission is to advance the technological landscape of Pakistan
+              by nurturing talent and fostering innovation. We believe that
+              technology, when effectively harnessed, drives growth, enhances
+              productivity, and creates a lasting impact on individuals,
+              organizations, and society at large.
             </p>
           </div>
         </div>
@@ -69,7 +68,7 @@ function About() {
 
       {/* 3RD SECTION */}
 
-      <div className='flex justify-center bg-[#ffffff] pt-[50px] pb-9 mt-9'>
+      {/* <div className='flex justify-center bg-[#ffffff] pt-[50px] pb-9 mt-9'>
         <div className='w-[90%] flex flex-col md:flex-row gap-4 md:gap-0 items-center '>
           <div className='w-[100%] md:w-[50%] flex flex-col gap-10'>
             <h1 className='text-[34px] md:text-[44px] font-bold font-poppins'>
@@ -77,29 +76,35 @@ function About() {
             </h1>
 
             <p className='font-poppins text-[16px] md:text-[18px]'>
-            Our mission is to advance the technological landscape of Pakistan by nurturing talent and fostering innovation. We believe that technology, when effectively harnessed, drives growth, enhances productivity, and creates a lasting impact on individuals, organizations, and society at large.
-
+              Our mission is to advance the technological landscape of Pakistan
+              by nurturing talent and fostering innovation. We believe that
+              technology, when effectively harnessed, drives growth, enhances
+              productivity, and creates a lasting impact on individuals,
+              organizations, and society at large.
             </p>
             <p className='font-poppins text-[16px] md:text-[18px]'>
-            Nixortech Solutions is dedicated to excellence in education, striving to deliver top-quality training that is customer-centric and industry-relevant. Our commitment to excellence ensures that our students are not just skilled professionals but also future leaders in the global technology space.
+              Nixortech Solutions is dedicated to excellence in education,
+              striving to deliver top-quality training that is customer-centric
+              and industry-relevant. Our commitment to excellence ensures that
+              our students are not just skilled professionals but also future
+              leaders in the global technology space.
             </p>
-           
           </div>{' '}
           <div className='w-[100%] h-[500px] md:w-[50%] flex justify-center items-center'>
             <img
-            alt=''
+              alt=''
               src={process.env.PUBLIC_URL + '/siriqbal.jpg'}
               className='w-full h-full object-contain'
             />
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* 4TH SECTION */}
       <div className='w-[100%] bg-[#f8f9fa] flex justify-center'>
         <div className='w-[90%] pt-5 pb-5  flex flex-col items-center'>
           <h2 className='font-poppins font-bold text-[22px] md:text-[30px]'>
-            Aptech cover every major cities in world.
+            Nixortech cover every major cities in world.
           </h2>
           <div className='w-full flex justify-between flex-col gap-4 md:gap-0 md:flex-row mt-5'>
             <div className='w-[100%] md:w-[33.3%] flex flex-col items-center'>
@@ -132,7 +137,7 @@ function About() {
             <div className='inline text-[#008000]'>36</div>
             centers across
             <img
-             alt=''
+              alt=''
               src={process.env.PUBLIC_URL + '/pakistan.png'}
               className='w-[30px] object-contain inline'
             />
@@ -144,7 +149,10 @@ function About() {
               🏆 Acheivement
             </h1>
             <p className='text-[16px] font-poppins'>
-            With a rapidly growing footprint, Nixortech Solutions has emerged as a trusted name in tech education. Our commitment to quality has been recognized by industry experts, and we take pride in our contributions to the tech ecosystem
+              With a rapidly growing footprint, Nixortech Solutions has emerged
+              as a trusted name in tech education. Our commitment to quality has
+              been recognized by industry experts, and we take pride in our
+              contributions to the tech ecosystem
             </p>
           </div>
 
@@ -164,7 +172,7 @@ function About() {
             </div>{' '}
             <div className='w-[100%] md:w-[50%] h-[400px] flex justify-end'>
               <img
-               alt=''
+                alt=''
                 src={process.env.PUBLIC_URL + '/award.jpg'}
                 className='object-contain w-full h-full rounded-md'
               />
@@ -173,7 +181,7 @@ function About() {
           <div className='flex flex-col md:flex-row shadow-sm hover:shadow-lg w-[100%] md:w-[70%] gap-5 md:gap-0 bg-white  mt-9 '>
             <div className='w-[100%] md:w-[50%] h-[400px] flex justify-end'>
               <img
-               alt=''
+                alt=''
                 src={process.env.PUBLIC_URL + '/aboutimg.jpg'}
                 className='object-contain w-full h-full rounded-md'
               />
@@ -188,33 +196,45 @@ function About() {
             </div>
           </div>
 
-
-
-
           <div className='flex flex-col md:flex-row shadow-sm hover:shadow-lg w-[100%] md:w-[70%] gap-5 md:gap-0 bg-white  mt-9 mb-9 '>
             <div className='w-[100%]   md:p-5 p-4  flex flex-col gap-4 md:gap-0  '>
               <div className='flex flex-col  gap-5'>
                 <h3 className='font-poppins font-bold text-[18px] md:text-[20px]'>
-                Aptech Pakistan Won 🏆 awards
+                  Nixortech Won 🏆 awards
                 </h3>
                 <p className='font-poppins text-[16px]'>
-                 <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>(2018)</span> -  Brands of the Year
+                  <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>
+                    (2018)
+                  </span>{' '}
+                  - Brands of the Year
                 </p>
                 <p className='font-poppins text-[16px]'>
-                 <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>(2017)</span> -  International Star for Leadership in Quality
+                  <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>
+                    (2017)
+                  </span>{' '}
+                  - International Star for Leadership in Quality
                 </p>
                 <p className='font-poppins text-[16px]'>
-                 <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>(2016)</span> -  Who's Who Award
+                  <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>
+                    (2016)
+                  </span>{' '}
+                  - Who's Who Award
                 </p>
                 <p className='font-poppins text-[16px]'>
-                 <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>(2014)</span> -  Brand Scientist Award
+                  <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>
+                    (2014)
+                  </span>{' '}
+                  - Brand Scientist Award
                 </p>
                 <p className='font-poppins text-[16px]'>
-                 <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>(2013)</span> -  Best Achievement, at APTECH World Leadership Summit, Thailand
+                  <span className='bg-[#ffbe11] pl-3 pr-3 pt-1 pb-1 text-white'>
+                    (2013)
+                  </span>{' '}
+                  - Best Achievement, at APTECH World Leadership Summit,
+                  Thailand
                 </p>
               </div>
             </div>{' '}
-          
           </div>
         </div>
       </div>
